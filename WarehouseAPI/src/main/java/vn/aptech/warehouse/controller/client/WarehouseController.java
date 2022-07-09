7eabf0afdc43e8e5403b0ca3423fe92a06a95695
thienan1512002 <72856151+thienan1512002@users.noreply.github.com>
2022-07-09 22:09:17 +0700

    Update warehouse template

--- WarehouseAPI/src/main/java/vn/aptech/warehouse/controller/client/WarehouseController.java
+++ WarehouseAPI/src/main/java/vn/aptech/warehouse/controller/client/WarehouseController.java
@@ -4,10 +4,16 @@
  */
 package vn.aptech.warehouse.controller.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vn.aptech.warehouse.entity.Warehouse;
+import vn.aptech.warehouse.service.WarehouseService;
 
 /**
  *
@@ -16,8 +22,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/warehouse")
 public class WarehouseController {
+    
+    @Autowired
+    private WarehouseService service;
+    
      @GetMapping(value="")
     public String index(Model model){
+        model.addAttribute("warehouses", service.findAll());
         return "warehouse/index";
     }
+    
+    @PostMapping(value="/save")
+    public ResponseEntity save(@RequestBody Warehouse warehouse){
+        Warehouse wh = service.save(warehouse);
+        return ResponseEntity.ok(200);
+    } 
 }
